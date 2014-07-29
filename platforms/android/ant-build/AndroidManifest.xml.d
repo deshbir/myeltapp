fc13a3e6d3808623e8d237092ee38a1fb326fd69
manavmanocha <manav.manocha@comprotechnologies.com>
2014-07-29 18:30:30 +0530

    Deleting unwanted files

--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-F:\myeltapp\platforms\android\ant-build\AndroidManifest.xml : \
-F:\myeltapp\platforms\android\AndroidManifest.xml \
--- platforms/android/ant-build/MyeltApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-F:\myeltapp\platforms\android\ant-build\MyeltApp-debug-unaligned.apk : \
-F:\myeltapp\platforms\android\ant-build\MyeltApp.ap_ \
-F:\myeltapp\platforms\android\ant-build\classes.dex \
-F:\myeltapp\platforms\android\CordovaLib\ant-build\classes.jar \
