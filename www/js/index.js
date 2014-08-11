262d11f9969213b67d6d869c52e4c4ed919fe7a3
nikhilagw <nikhil.aggarwal@comprotechnologies.com>
2014-08-11 16:12:12 +0530

    Added playback support.

--- www/js/index.js
+++ www/js/index.js
@@ -1,55 +1,97 @@
-var url  = "http://192.168.1.59:3714";
-
-document.addEventListener("deviceready", onDeviceReady, false);
-	function onDeviceReady() {
-		var iframe = document.getElementById('MyELTIframe');
-		iframe.src= url;
-		
-		var initializeIframe = true;
-		
-		iframe.addEventListener("load", 
-			function(event) {
-				if(initializeIframe) {
-					window.frames[0].postMessage({'location' : 'device'},url);
-					initializeIframe = false;
-				}
-			}, false);
-		//Listens for events via postMessage
-		window.addEventListener("message", function(event) {
-			if (event.data.operation == "startRecording") {
-     			recordAudio();
-     		}
-			if (event.data.operation == "stopRecording") {
-     			stopRecordAudio()
-     		}
-			if (event.data.operation == "scoreAudio") {
-     			scoreAudio(event.data.clientId);
-     		}
-     		if ((event.data).indexOf("pdf") > -1) {
-     			window.open(event.data, '_system', 'location=yes');
-     		}
-     		if ((event.data).indexOf("Help") > -1) {
-     			window.open(event.data, '_self', 'location=yes');
-     		}
-		});
+(function(){
+	
+	/* Global Variables. */
+	var url  = "http://192.168.1.59:3714";
+	var mediaObject;
+	var recordPlayTime;
+	var directoryName = "MyELT";
+	var defaultAduioFileName ="audioFile.wav";
+	var filePath = directoryName + "/" + defaultAduioFileName;
+	
+	/******************************************* Helper Functions Starts *********************************************/
+	//function call if file system fails
+	var fsFail = function(error) {
+	   alert("failed with error code: " + error.code);
+	};
+	//function call if directory fails 
+	var dirFail = function(error) {
+	   alert("Directory error code: " + error.code);
+	};
+	//function call if directory successfully created
+	var getDirSuccess = function() {
+		console.log("Directory created Successfully");
+	};
+	
+	var createDirectory = function(){
+		//function call to create a directory(if it does not exist)
+		var gotFileSystem = function (fileSystem) {
+	    	fileSystem.root.getDirectory(directoryName, { create: true, exclusive: false }, getDirSuccess, dirFail);
+		};
+	    // get file system to copy or move audio file to a specified location
+	    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFileSystem, fsFail);
 	}
-    
-    //Score Audio
-	function scoreAudio(clientId) {
-		//This function is called if file system fails
-	    var fsFail1 = function(error) {
-	        alert("failed with error code: " + error.code);
-	    };
-	    //This function is called if directory system fails
-	    var dirFail1 = function(error) {
-	        alert("Directory error code: " + error.code);
-	    };
+	
+	var onSuccess = function() {
+		console.log("recordAudio():Audio Success");
+	}
+	
+	// onError Callback 
+	var onError = function(error) {
+		alert('code: '    + error.code    + '\n' + 
+	     'message: ' + error.message + '\n');
+	}
+
+	var setAudioPosition = function(position) {
+		window.frames[0].document.getElementById('audio_position').innerHTML = position;
+	}
+	
+	/******************************************* Helper Functions Ends *********************************************/
+	
+	
+	/******************************************* Audio Specific Starts *********************************************/
+	/* Record button will call this function */
+	var recordAudio = function() {
+		createDirectory();
+		mediaObject = new Media(filePath, onSuccess, onError);
+		// Record audio
+		mediaObject.startRecord();
+		/*recordPlayTime = 0;
+		recInterval = setInterval(function() {
+	    	recordPlayTime = recordPlayTime + 1;
+	    	setAudioPosition(recordPlayTime + " sec");
+	    	}, 1000);*/
+	}
+	
+	/* Stop button will call this function and saves file at a specified location */
+	var stopRecordAudio = function() {
+		 if (mediaObject) {
+		 	//clearInterval(recInterval);
+		 	//function to stop recording
+	        mediaObject.stopRecord();
+	    }
+	}
+	
+	/* Start playback */
+	var startPlayback = function() {
+		mediaObject = new Media(filePath, onSuccess, onError);
+		mediaObject.play();
+	}
+	
+	/* Stop playback */
+	var stopPlayback = function() {
+		if (mediaObject) {
+			mediaObject.stop();
+        }
+	}
+	
+	/* Score Audio */
+	var scoreAudio = function(clientId) {
 	    //This function is called if directory successfully created
 		var getDirSuccess1 = function(entry) {
-	     	entry.file(gotFile, fail1);
+	     	entry.file(gotFile, fail);
 	    };
 	    //This function is called if error occurs while fetching file from directory
-		var fail1 = function(error) {
+		var fail = function(error) {
 	        alert("An error has occurred: Code = " = error.code);
 	    };
 	    //This function is called if file is successfully found
@@ -78,72 +120,50 @@ document.addEventListener("deviceready", onDeviceReady, false);
 		    reader.readAsArrayBuffer(file);
 	    };
 	    var gotFileSystem1 = function (fileSystem) {
-	    	fileSystem.root.getFile("MyELT1/testAudio.wav", null, getDirSuccess1, dirFail1);
+	    	fileSystem.root.getFile(filePath, null, getDirSuccess1, dirFail);
 	    };
 	    // get file system to copy or move audio file to a specified folder
-	    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFileSystem1, fsFail1);
+	    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFileSystem1, fsFail);
 	    
 	}
-	//Record button will call this function
-	var mediaRec;
-	var recTime;
-	function recordAudio() {
-		createDirectory();
-		audioFile="MyELT1/testAudio.wav";
-		mediaRec = new Media(audioFile, onSuccess, onError);
+	/******************************************* Audio Specific Ends *********************************************/
 	
-		// Record audio
-		mediaRec.startRecord();
-		recTime = 0;
-		recInterval = setInterval(function() {
-	    	recTime = recTime + 1;
-	    	setAudioPosition(recTime + " sec");
-	    	}, 1000);
-	}
-	function onSuccess() {
-		console.log("recordAudio():Audio Success");
-	}
+	/******************************************* Device Ready Specific Starts *********************************************/
 	
-	// onError Callback 
-	function onError(error) {
-		alert('code: '    + error.code    + '\n' + 
-	     'message: ' + error.message + '\n');
+	var onDeviceReady = function() {
+		var iframe = document.getElementById('MyELTIframe');
+		iframe.src= url;
+		
+		var initializeIframe = true;
+		
+		iframe.addEventListener("load", 
+			function(event) {
+				if(initializeIframe) {
+					window.frames[0].postMessage({'location' : 'device'},url);
+					initializeIframe = false;
+				}
+			}, false);
+		//Listens for events via postMessage
+		window.addEventListener("message", function(event) {
+			if (event.data.operation == "startRecording") {
+     			recordAudio();
+     		}
+			if (event.data.operation == "stopRecording") {
+     			stopRecordAudio();
+     		}
+			if (event.data.operation == "scoreAudio") {
+     			scoreAudio(event.data.clientId);
+     		}
+			if (event.data.operation == "startPlayback") {
+     			startPlayback();
+     		}
+			if (event.data.operation == "stopPlayback") {
+				stopPlayback();
+     		}
+		});
 	}
-
-	function createDirectory(){
-		 //function call if file system fails
-	    var fsFail = function(error) {
-	        alert("failed with error code: " + error.code);
+	document.addEventListener("deviceready", onDeviceReady, false);
+	/******************************************* Device Ready Specific Ends *********************************************/
 	
-	    };
-	    //function call if directory fails 
-		var dirFail = function(error) {
-	        alert("Directory error code: " + error.code);
+})();
 	
-	    };
-	    //function call if directory successfully created
-		var getDirSuccess = function() {
-			console.log("Directory created Successfully");
-	     	
-	    };
-	    //function call to create a directory(if it does not exist)
-		 var gotFileSystem = function (fileSystem) {
-		    	fileSystem.root.getDirectory("MyELT1", { create: true, exclusive: false }, getDirSuccess, dirFail);
-		    };
-	    // get file system to copy or move audio file to a specified location
-	    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFileSystem, fsFail);
-		    
-	}
-	
-	//Stop button will call this function and saves file at a specified location
-	function stopRecordAudio() {
-		 if (mediaRec) {
-		 	clearInterval(recInterval);
-		 	//function to stop recording
-	        mediaRec.stopRecord();
-	    }
-	}
-	
-	function setAudioPosition(position) {
-		window.frames[0].document.getElementById('audio_position').innerHTML = position;
-	}
