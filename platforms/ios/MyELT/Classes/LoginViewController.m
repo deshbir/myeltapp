96159cfbada0f68eb2729966a07646f8601af7bb
manav.manocha <manav.manocha@comprotechnologies.com>
2014-09-30 19:28:12 +0530

    Updates to use new MyELT login API in iOS

--- platforms/ios/MyELT/Classes/LoginViewController.m
+++ platforms/ios/MyELT/Classes/LoginViewController.m
@@ -13,16 +13,26 @@
 
 @property (nonatomic, weak) IBOutlet UITextField *userName;
 @property (nonatomic, weak) IBOutlet UITextField *password;
+@property (nonatomic) NSURLSession *session;
 
 @end
 
 @implementation LoginViewController
 
+//MyELT Server URL for HTTP/REST calls
+NSString * const SERVER_URL = @"http://myelt3.comprotechnologies.com";
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        NSURLSessionConfiguration *config =
+        [NSURLSessionConfiguration defaultSessionConfiguration];
+        
+        //Initialize session with default configuration
+        _session = [NSURLSession sessionWithConfiguration:config
+                                                 delegate:nil
+                                            delegateQueue:nil];
     }
     return self;
 }
@@ -64,9 +74,61 @@
 
 - (void)switchToMainPage
 {
-    MainViewController *mainVC = [[MainViewController alloc] initWithUserName:self.userName.text password:self.password.text];
-    [self presentViewController:mainVC animated:NO completion:nil];
+    NSString *requestURLString = [NSString stringWithFormat:@"%@/ilrn/api/logincheck?u=%@&p=%@", SERVER_URL, self.userName.text, self.password.text];
+    
+    NSURL *requestURL = [NSURL URLWithString:requestURLString];
+    
+    NSURLRequest *loginRequest = [NSURLRequest requestWithURL:requestURL];
+    
+    NSURLSessionDataTask *dataTask =
+    [self.session dataTaskWithRequest:loginRequest
+                    completionHandler:
+     ^(NSData *data, NSURLResponse *response, NSError *error) {
+         //Run this snippet on main UI thread
+         dispatch_async(dispatch_get_main_queue(), ^{
+             if (error) {
+                [self showAlert:@"Something went wrong. Please try again later." title:@"Error"];
+             }
+             else {
+                 if ([data length] > 0) {
+                     
+                     NSInteger statusCode = [(NSHTTPURLResponse *)response statusCode];
+                     
+                     if (statusCode != 200) {
+                         [self showAlert:@"Something went wrong. Please try again later." title:@"Error"];
+                     } else {
+                         NSDictionary *jsonObject = [NSJSONSerialization JSONObjectWithData:data                                                                                options:0 error:nil];
+                         
+                         NSDictionary *responseJSON  = jsonObject[@"response"];
+                         NSString *responseStatus  = responseJSON[@"status"];
+                         
+                         if ([responseStatus isEqual:@"success"]) {
+                             MainViewController *mainVC = [[MainViewController alloc] initWithUserName:self.userName.text password:self.password.text];
+                             [self presentViewController:mainVC animated:NO completion:nil];
+                         } else {
+                             [self showAlert:@"Incorrect Username or Password." title:@"Error"];
+                         }
+                     }
+                     
+                 } else {
+                     [self showAlert:@"Something went wrong. Please try again later." title:@"Error"];
+                 }
+             }
+         });
+     }];
+    [dataTask resume];
 }
+
+- (void)showAlert:(NSString *)messageStr title:(NSString *)titleStr
+{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:titleStr
+                                                    message:messageStr
+                                                   delegate:self
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil];
+    [alert show];
+}
+
 
 
 
